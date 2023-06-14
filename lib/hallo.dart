3f069757fc4f743119d286a57f861bf1acf833ff
lianliu <liulian8233@gmail.com>
2023-06-14 22:56:35 +0800

    delete

--- lib/hallo.dart
+++ lib/hallo.dart
@@ -165,35 +165,6 @@ class GeneratorPage extends StatelessWidget {
 
 // ...
 
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text(
-            pair.asLowerCase,
-            style: style,
-            semanticsLabel: "${pair.first} ${pair.second}",
-        ),
-      ),
-    );
-  }
-}
-
-
 class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
